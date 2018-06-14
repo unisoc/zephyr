34a0f5d3eea31014b2240bae89d16b826ae8973b
Dong Xiang <dong.xiang@unisoc.com>
2018-06-14 10:05:53 +0800

    dts: Add dts configration for Unisoc uwp566x chip.
    dts files for Unisoc uwp566x chip.
    
    Signed-off-by: Dong Xiang <dong.xiang@unisoc.com>

---
+++ dts/arm/unisoc/mem.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018, UNISOC Incorporated
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __DT_UNISOC_MEM_H
+#define __DT_UNISOC_MEM_H
+
+#define __SIZE_K(x) (x * 1024)
+#define __SIZE_M(x) (x * 1024 * 1024)
+
+#if defined(CONFIG_SOC_UWP566x)
+#define DT_SRAM_START		0x180000
+#define DT_SRAM_SIZE		__SIZE_K(256)
+#define DT_UART0_START		0x40038000
+#define DT_UART1_START		0x40040000
+#define DT_UART2_START		0x40838000
+#define DT_UART_SIZE		0x30
+#define DT_FLASH_START		0x2000000
+#define DT_FLASH_SIZE		__SIZE_M(4)
+#else
+#error "Flash and RAM sizes not defined for this chip"
+#endif
+
+#endif /* __DT_UNISOC_MEM_H */
