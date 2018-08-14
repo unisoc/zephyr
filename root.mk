ab586e64c67c6a397749215ab58f2c8dccb0e316
Keguang Zhang <keguang.zhang@spreadtrum.com>
2018-08-14 16:05:15 +0800

    Bug#909013 - root.mk: amend the top Makfile
    Amend the top Makfile.
    1. use Makefile function to reduce targets
    2. add images dir for preparing PAC
    3. generate config.fat
    
    [self test   ]<board, Test cases summary>:
    [change_type ] bringup --> sprd_chip_support
    
    Change-Id: I5764473f6e393ac02fecc88b238bed4be8345e44
    Signed-off-by: Keguang Zhang <keguang.zhang@spreadtrum.com>

--- root.mk
+++ root.mk
@@ -7,9 +7,7 @@
 ARCH		:= arm
 BOARD		?= uwp566x_evb
 BOOT		:= mcuboot
-KERNEL		:= kernel
-XIP_BOOT	:= xip_mcuboot
-XIP_KERNEL	:= xip_kernel
+KERNEL		:= zephyr
 
 # Directories and Files
 ################################################################
@@ -19,22 +17,23 @@ PRJDIR		:= $(PWD)
 
 app_DIR		:= $(PRJDIR)/app
 boot_DIR	:= $(PRJDIR)/$(BOOT)
-build_DIR	:= $(PRJDIR)/build
 fdl_DIR		:= $(PRJDIR)/fdl
 kernel_DIR	:= $(PRJDIR)/$(KERNEL)
 toolchain_DIR	:= $(PRJDIR)/toolchain
 
-SIGNING_KEY ?= $(boot_DIR)/root-rsa-2048.pem
-BOOT_HEADER_LEN = 0x200
-FLASH_ALIGNMENT = 8
+BUILD_DIR		:= $(PRJDIR)/build
+boot_BUILD_DIR		:= $(BUILD_DIR)/$(BOOT)
+kernel_BUILD_DIR	:= $(BUILD_DIR)/$(KERNEL)
+boot_debug_BUILD_DIR	:= $(BUILD_DIR)/$(BOOT)_debug
+kernel_debug_BUILD_DIR	:= $(BUILD_DIR)/$(KERNEL)_debug
 
-IMGTOOL = $(boot_DIR)/scripts/imgtool.py
-ASSEMBLE = $(boot_DIR)/scripts/assemble.py
+BOOT_BIN	:= $(boot_BUILD_DIR)/$(KERNEL)/$(KERNEL).bin
+KERNEL_BIN	:= $(kernel_BUILD_DIR)/$(KERNEL)/$(KERNEL).bin
 
-kernel_BUILD_DIR	:= $(build_DIR)/$(KERNEL)
-boot_BUILD_DIR	:= $(build_DIR)/$(BOOT)
-xip_kernel_BUILD_DIR	:= $(build_DIR)/$(XIP_KERNEL)
-xip_boot_BUILD_DIR	:= $(build_DIR)/$(XIP_BOOT)
+DIST_DIR	:= $(PRJDIR)/build/images
+BOOT_DIST_BIN	:= $(DIST_DIR)/$(BOOT)-pubkey.bin
+KERNEL_DIST_BIN	:= $(DIST_DIR)/$(KERNEL)-signed-ota.bin
+CONFIG_DIST_BIN	:= $(DIST_DIR)/config.fat
 
 cmake_FILE	:= $(toolchain_DIR)/cmake-3.8.2-Linux-x86_64.sh
 cmake_DIR	:= $(basename $(cmake_FILE))
@@ -42,32 +41,12 @@ export PATH	:=$(cmake_DIR)/bin:$(PATH)
 
 sdk_FILE	:= $(toolchain_DIR)/zephyr-sdk-0.9.3-setup.run
 sdk_DIR		:= $(basename $(sdk_FILE))
+
 export ZEPHYR_TOOLCHAIN_VARIANT	:= zephyr
 export ZEPHYR_SDK_INSTALL_DIR	:= $(sdk_DIR)
 
-# Targets
-################################################################
-DEFAULT_TARGETS		:= cmake sdk kernel boot
-XIP_TARGETS			:= cmake sdk xip_kernel xip_boot
-CLEAN_TARGETS		:= $(addsuffix -clean,$(DEFAULT_TARGETS))
-DISTCLEAN_TARGETS	:= $(addsuffix -distclean,$(DEFAULT_TARGETS))
-
-.PHONY: all
-all: $(DEFAULT_TARGETS)
-
-.PHONY: xip
-xip: $(XIP_TARGETS)
-
-.PHONY: clean
-clean:
-	if [ -d $(kernel_BUILD_DIR) ]; then make clean -C $(kernel_BUILD_DIR); fi
-	if [ -d $(boot_BUILD_DIR) ]; then make clean -C $(boot_BUILD_DIR); fi
-	if [ -d $(xip_kernel_BUILD_DIR) ]; then make clean -C $(xip_kernel_BUILD_DIR); fi
-	if [ -d $(xip_boot_BUILD_DIR) ]; then make clean -C $(xip_boot_BUILD_DIR); fi
-
-.PHONY: distclean
-distclean:
-	if [ -d $(build_DIR) ]; then rm -rf $(build_DIR); fi
+IMGTOOL = $(boot_DIR)/scripts/imgtool.py
+ASSEMBLE = $(boot_DIR)/scripts/assemble.py
 
 # Macros
 ################################################################
@@ -79,7 +58,7 @@ TERM_RESET := $(shell tput rmso 2>/dev/null)
 # Macro of Extracting cmake
 # $(1): cmake file
 # $(2): cmake directory
-define EXTRACT_CMAKE
+define EXTRACT_cmake
 	cd $(toolchain_DIR); \
 	yes | sh $(1) | cat; \
 	cmake --version; \
@@ -89,96 +68,123 @@ endef
 # Macro of Extracting Zephyr SDK
 # $(1): SDK file
 # $(2): SDK directory
-define EXTRACT_SDK
+define EXTRACT_sdk
 	sh $(1) -- -d $(2); \
 	touch $(2);
 endef
 
-# Respective Targets
-################################################################
-.PHONY: cmake
-cmake:
-	@ if [ ! -d $($@_DIR) ]; then \
-		$(call MESSAGE,"Preparing $@"); \
-		$(call EXTRACT_CMAKE,$(notdir $($@_FILE)),$($@_DIR)) \
+# Macro of Preparing Building Environment
+# $(1): Target
+define MAKE_ENV
+.PHONY: $(1)
+$(1):
+	@ if [ ! -d $($(1)_DIR) ]; then \
+		$(call MESSAGE,"Preparing $(1)"); \
+		$(call EXTRACT_$(1),$($(1)_FILE),$($(1)_DIR)) \
 	fi
-	@ if ( find $($@_FILE) -newer $($@_DIR) | grep -q $($@_FILE) ); then \
-		$(call MESSAGE,"Updating $@"); \
-		rm -rf $($@_DIR); \
-		$(call EXTRACT_CMAKE,$(notdir $($@_FILE)),$($@_DIR)) \
+	@ if ( find $($(1)_FILE) -newer $($(1)_DIR) | grep -q $($(1)_FILE) ); then \
+		$(call MESSAGE,"Updating $(1)"); \
+		rm -rf $($(1)_DIR); \
+		$(call EXTRACT_$(1),$($(1)_FILE),$($(1)_DIR)) \
 	fi
+endef
 
-.PHONY: sdk
-sdk:
-	@ if [ ! -d $($@_DIR) ]; then \
-		$(call MESSAGE,"Preparing $@"); \
-		$(call EXTRACT_SDK,$($@_FILE),$($@_DIR)) \
-	fi
-	@ if ( find $($@_FILE) -newer $($@_DIR) | grep -q $($@_FILE) ); then \
-		$(call MESSAGE,"Updating $@"); \
-		rm -rf $($@_DIR); \
-		$(call EXTRACT_SDK,$($@_FILE),$($@_DIR)) \
-	fi
-
-.PHONY: kernel
-kernel:
-	echo $($@_BUILD_DIR)
-	@ if [ ! -d $($@_BUILD_DIR) ]; then mkdir -p $($@_BUILD_DIR); fi
-	(source $($@_DIR)/zephyr-env.sh && cd $($@_BUILD_DIR) && \
-	if [ ! -f ./Makefile ] ; then \
-	cmake -DBOARD=$(BOARD) $($@_DIR)/samples/repeater/ ;\
-	fi &&\
+# Macro of Building Targets
+# $(1): Target
+# $(2): Target suffix
+# $(3): Dir of main
+define MAKE_TARGET
+.PHONY: $(if $(2),$(1)-$(2),$(1))
+$(if $(2),$(1)-$(2),$(1)): $(ENV_TARGETS)
+	@ $(call MESSAGE,"Building $(1)")
+	@ if [ ! -d $($(if $(2),$(1)_$(2),$(1))_BUILD_DIR) ]; then mkdir -p $($(if $(2),$(1)_$(2),$(1))_BUILD_DIR); fi
+	(source $(kernel_DIR)/zephyr-env.sh && cd $($(if $(2),$(1)_$(2),$(1))_BUILD_DIR) && \
+	if [ ! -f Makefile ] ; then cmake -DBOARD=$(BOARD) $(if $(2),,-DCONF_FILE=prj_xip.conf) $(3); fi && \
 	make \
 	)
+endef
 
-.PHONY: boot
-boot:
-	echo $($@_BUILD_DIR)
-	@ if [ ! -d $($@_BUILD_DIR) ]; then mkdir -p $($@_BUILD_DIR); fi
-	(source $(kernel_DIR)/zephyr-env.sh && cd $($@_BUILD_DIR) && \
-	if [ ! -f ./Makefile ] ; then \
-	cmake -DBOARD=$(BOARD) $(boot_DIR)/boot/zephyr/ ; \
-	fi &&\
-	make \
-	)
+# Macro of Cleaning Targets
+# $(1): Target
+# $(2): Target suffix
+# $(3): .config
+define CLEAN_TARGET
+.PHONY: $(if $(2),$(1)-$(2),$(1))
+$(if $(2),$(1)-$(2),$(1)):
+	@ $(call MESSAGE,"Cleaning $(1)")
+	@ if [ -d $($(1)_BUILD_DIR) ]; then make -C $($(1)_BUILD_DIR) $(2); fi
+endef
 
-.PHONY: xip_kernel
-xip_kernel:
-	echo $($@_BUILD_DIR)
-	@ if [ ! -d $($@_BUILD_DIR) ]; then mkdir -p $($@_BUILD_DIR); fi
-	(source $(kernel_DIR)/zephyr-env.sh && cd $($@_BUILD_DIR) && \
-	if [ ! -f ./Makefile ] ; then \
-	cmake -DBOARD=$(BOARD) -DCONF_FILE=prj_xip.conf $(kernel_DIR)/samples/repeater/ ; \
-	fi &&\
-	make \
-	)
+SIGNING_KEY	?= $(boot_DIR)/root-rsa-2048.pem
+BOOT_HEADER_LEN	:= 0x200
+FLASH_ALIGNMENT	:= 8
+
+# Macro of Signing KERNEL Image
+# $(1): input file
+# $(2): output file
+define SIGN_KERNEL_IMAGE
 	$(IMGTOOL) sign \
 		--key $(SIGNING_KEY) \
 		--header-size $(BOOT_HEADER_LEN) \
 		--align $(FLASH_ALIGNMENT) \
 		--version 1.2 \
 		--included-header \
-		$($@_BUILD_DIR)/zephyr/zephyr.bin \
-		signed-kernel.bin
+		$(1) $(2)
+endef
 
-.PHONY: xip_boot
-xip_boot:
-	echo $($@_BUILD_DIR)
-	@ if [ ! -d $($@_BUILD_DIR) ]; then mkdir -p $($@_BUILD_DIR); fi
-	(source $(kernel_DIR)/zephyr-env.sh && cd $($@_BUILD_DIR) && \
-	if [ ! -f ./Makefile ] ; then \
-	cmake -DBOARD=$(BOARD) -DCONF_FILE=prj_xip.conf $(boot_DIR)/boot/zephyr/ ; \
-	fi &&\
-	make \
-	)
+# Macro of Generating Config Image
+# $(1): Path of config image
+# $(n): Name of config files
+define GEN_CONFIG_IMAGE
+	@ rm -f $(1)
+	@ mkfs.vfat -a -S 4096 -v -C $(1) 128
+	@ (cd $(HWCONFIG_DIR) && \
+	if [ -f $(2) ]; then mcopy -i $(1) $(2) ::; fi && \
+	if [ -f $(3) ]; then mcopy -i $(1) $(3) ::; fi && \
+	if [ -f $(4) ]; then mcopy -i $(1) $(4) ::; fi)
+endef
+
+# Targets
+################################################################
+ENV_TARGETS		:= cmake sdk
+DEFAULT_TARGETS		:= boot kernel
+DEBUG_TARGETS		:= boot-debug kernel-debug
+ALL_TARGETS		:= $(DEFAULT_TARGETS) $(DEBUG_TARGETS)
+CLEAN_TARGETS		:= $(addsuffix -clean,$(ALL_TARGETS))
+
+.PHONY: all
+all: $(DEFAULT_TARGETS)
+	@ if [ ! -d $(DIST_DIR) ]; then install -d $(DIST_DIR); fi
+	install $(BOOT_BIN) $(BOOT_DIST_BIN)
+	$(call SIGN_KERNEL_IMAGE,$(KERNEL_BIN),$(KERNEL_DIST_BIN))
+	$(call GEN_CONFIG_IMAGE,$(CONFIG_DIST_BIN),wifi_board_config.ini,bt_configure_pskey.ini,bt_configure_rf.ini)
 
 .PHONY: debug
-debug: check kernel
+debug: $(DEBUG_TARGETS)
 	@killall JLinkGDBServer || true
 	JLinkGDBServer -device Cortex-M4 -endian little -if SWD -speed 8000 -jlinkscriptfile /opt/SEGGER/JLink/Samples/JLink/Scripts/SPRD_SC2355.JLinkScript &
 	${ZEPHYR_SDK_INSTALL_DIR}/sysroots/x86_64-pokysdk-linux/usr/bin/arm-zephyr-eabi/arm-zephyr-eabi-gdb $(kernel_BUILD_DIR)/zephyr/zephyr.elf
 
-check:
-	@if [ -z "$$ZEPHYR_BASE" ]; then echo "Zephyr environment not set up"; false; fi
-	@if [ -z "$$ZEPHYR_SDK_INSTALL_DIR" ]; then echo "Zephyr environment not set up"; false; fi
-	@if [ -z "$(BOARD)" ]; then echo "You must specify BOARD=<board>"; false; fi
+.PHONY: clean
+clean: $(CLEAN_TARGETS)
+
+.PHONY: distclean
+distclean:
+	@ if [ -d $(BUILD_DIR) ]; then rm -rf $(BUILD_DIR); fi
+
+# Respective Targets
+################################################################
+# Environment Targets
+$(foreach target,$(ENV_TARGETS),$(eval $(call MAKE_ENV,$(target))))
+
+# Build Targets
+$(eval $(call MAKE_TARGET,boot,,$(boot_DIR)/boot/zephyr))
+
+$(eval $(call MAKE_TARGET,kernel,,$(kernel_DIR)/samples/repeater))
+
+$(eval $(call MAKE_TARGET,boot,debug,$(boot_DIR)/boot/zephyr))
+
+$(eval $(call MAKE_TARGET,kernel,debug,$(kernel_DIR)/samples/repeater))
+
+# Clean Targets
+$(foreach target,$(ALL_TARGETS),$(eval $(call CLEAN_TARGET,$(target),clean)))
