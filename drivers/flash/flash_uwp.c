d68b9ea2647bc616251883bb612a4cc2d32160ad
Stephen.Zhang <Stephen.Zhang@spreadtrum.com>
2018-11-07 10:31:01 +0800

    drivers: flash: improve sfc freq to 104m in mcuboot.
    drivers: flash: improve sfc freq to 104m in mcuboot.
    
    Signed-off-by: Stephen.Zhang <Stephen.Zhang@spreadtrum.com>

--- drivers/flash/flash_uwp.c
+++ drivers/flash/flash_uwp.c
@@ -12,12 +12,14 @@
 #include <soc.h>
 #include <logging/sys_log.h>
 
-#include "uwp_hal.h"
+#include "hal_sfc.h"
+#include "hal_sfc_phy.h"
+#include "hal_sfc_hal.h"
 
 #define DEV_CFG(dev) \
-	((struct flash_uwp_config *)(dev)->config->config_info)
+	((struct flash_uwp_config *)((dev)->config->config_info))
 #define DEV_DATA(dev) \
-	((struct flash_uwp_data * const)(dev)->driver_data)
+	((struct flash_uwp_data * const)((dev)->driver_data))
 
 #define FLASH_WRITE_BLOCK_SIZE 0x1
 
@@ -72,20 +74,14 @@ static int flash_uwp_read(struct device *dev, off_t offset, void *data,
 			    size_t len)
 {
 	int ret = 0;
-	//struct flash_uwp_config *cfg = DEV_CFG(dev);
-	//struct spi_flash *flash = &(cfg->flash);
 
 	if (!len) {
 		return 0;
 	}
 
-	SYS_LOG_WRN("flash read: 0x%x len: 0x%x", offset, len);
 	flash_uwp_lock(dev);
 
-	//ret = flash->read(flash, offset, data, len, READ_SPI_FAST);
-	//ret = flash->read(flash, offset, data, len, READ_SPI);
 	memcpy(data, (void *)((u32_t)CONFIG_FLASH_BASE_ADDRESS + offset), len);
-
 
 	flash_uwp_unlock(dev);
 
@@ -137,7 +133,7 @@ static const struct flash_driver_api flash_uwp_api = {
 	.write = flash_uwp_write,
 	.read = flash_uwp_read,
 #ifdef CONFIG_FLASH_PAGE_LAYOUT
-//	.page_layout = flash_uwp_page_layout,
+	.page_layout = flash_uwp_page_layout,
 #endif
 #ifdef FLASH_WRITE_BLOCK_SIZE
 	.write_block_size = FLASH_WRITE_BLOCK_SIZE,
@@ -148,43 +144,28 @@ static const struct flash_driver_api flash_uwp_api = {
 #endif
 };
 
-#define SFC_CLK_OUT_DIV_1       (0x0)
-#define SFC_CLK_OUT_DIV_2       BIT(0)
-#define SFC_CLK_OUT_DIV_4       BIT(1)
-#define SFC_CLK_SAMPLE_DELAY_SEL    BIT(2)
-#define SFC_CLK_2X_EN           BIT(10)
-#define SFC_CLK_OUT_2X_EN       BIT(9)
-#define SFC_CLK_SAMPLE_2X_PHASE     BIT(8)
-#define SFC_CLK_SAMPLE_2X_EN        BIT(7)
-
-static int uwp_flash_init(struct device *dev)
+__ramfunc static int uwp_flash_init(struct device *dev)
 {
 	int ret = 0;
-
-#if 0
-	SFCDRV_ClkCfg(SFC_CLK_OUT_DIV_2 | SFC_CLK_OUT_2X_EN |
-			SFC_CLK_2X_EN | SFC_CLK_SAMPLE_2X_PHASE |
-			SFC_CLK_SAMPLE_2X_EN);
-
-	/*
-	 * cgm_sfc_1x_div: clk_sfc_1x = clk_src/(bit 9:8 + 1)
-	 * */
-	sci_write32(REG_AON_CLK_RF_CGM_SFC_1X_CFG, 0x100);
-	/* 0: xtal MHz 1: 133MHz 2: 139MHz 3: 160MHz 4: 208MHz
-	 * cgm_sfc_2x_sel: clk_sfc_1x source (bit 2:1:0)
-	 * */
-	sci_write32(REG_AON_CLK_RF_CGM_SFC_2X_CFG, 0x4);
 
 	struct flash_uwp_config *cfg = DEV_CFG(dev);
 	struct spi_flash *flash = &(cfg->flash);
 
+	spiflash_select_xip(TRUE);
+
+	SFCDRV_EnableInt();
+
+	spiflash_reset_anyway();
+
+	spiflash_set_clk();
+
 	ret = uwp_spi_flash_init(flash, &(cfg->params));
-	if(!ret) {
-		printk("uwp spi flash init failed.\n");
+	if (ret) {
+		printk("uwp spi flash init failed. ret:[%d]\n", ret);
 		return ret;
 	}
+
 	k_mutex_init(&cfg->lock);
-#endif
 
 	return ret;
 }
