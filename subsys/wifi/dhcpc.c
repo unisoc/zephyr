175e5daa866e375f7a9111aa6be5c20b70eb9b84
Keguang Zhang <keguang.zhang@unisoc.com>
2018-11-13 19:01:34 +0800

    subsys: wifi: refactor led part of WiFi manager
    Use generic LED interace instead of GPIO.
    
    Signed-off-by: Keguang Zhang <keguang.zhang@unisoc.com>

--- subsys/wifi/dhcpc.c
+++ subsys/wifi/dhcpc.c
@@ -22,7 +22,6 @@ static void wifimgr_dhcp_handler(struct net_mgmt_event_callback *cb,
 				 unsigned int mgmt_event, struct net_if *iface)
 {
 	int i = 0;
-	struct device *led;
 
 	if (mgmt_event != NET_EVENT_IPV4_ADDR_ADD)
 		return;
@@ -50,10 +49,7 @@ static void wifimgr_dhcp_handler(struct net_mgmt_event_callback *cb,
 		wifimgr_info("Router: %s\n",
 		       net_addr_ntop(AF_INET, gateway, buf, sizeof(buf)));
 
-		led = device_get_binding(WIFIMGR_LED_NAME);
-		if (led) {
-			led_on(led, WIFIMGR_LED_PIN3);
-		}
+		wifimgr_sta_led_on();
 	}
 }
 
@@ -73,7 +69,6 @@ void wifimgr_dhcp_start(void *handle)
 void wifimgr_dhcp_stop(void *handle)
 {
 	struct net_if *iface = (struct net_if *)handle;
-	struct device *led;
 
 	wifimgr_info("stop DHCP client\n");
 
@@ -81,8 +76,5 @@ void wifimgr_dhcp_stop(void *handle)
 
 	net_dhcpv4_stop(iface);
 
-	led = device_get_binding(WIFIMGR_LED_NAME);
-	if (led) {
-		led_off(led, WIFIMGR_LED_PIN3);
-	}
+	wifimgr_sta_led_off();
 }
