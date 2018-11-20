c152067b4ac85d0f0f4ab4865fda4b4747d33a9e
Dong Xiang <dong.xiang@unisoc.com>
2018-11-20 18:07:04 +0800

    soc: arm: include generated_dts_board.h by default.
    include generated_dts_board.h by default.
    
    Signed-off-by: Dong Xiang <dong.xiang@unisoc.com>

--- soc/arm/unisoc_uwp/uwp566x/soc.h
+++ soc/arm/unisoc_uwp/uwp566x/soc.h
@@ -3,8 +3,12 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#ifndef _UWP566X_SOC_H_
 
 #define __CM4_REV        0
 #define __MPU_PRESENT                  0 /* Zephyr has no MPU support */
 #define __Vendor_SysTickConfig         0 /* Default to standard SysTick */
 
+#include <generated_dts_board.h>
+
+#endif /* _UWP566X_SOC_H_ */
