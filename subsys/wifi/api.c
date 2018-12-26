ab543fb9fa14ff94c9d3f1710dd47e5d165b4bb9
Keguang Zhang <keguang.zhang@unisoc.com>
2018-12-26 14:18:58 +0800

    subsys: wifi: fix coding style issue reported by checkpatch.pl
    Fix coding style issue reported by checkpatch.pl.
    
    Signed-off-by: Keguang Zhang <keguang.zhang@unisoc.com>

--- subsys/wifi/api.c
+++ subsys/wifi/api.c
@@ -15,75 +15,7 @@ LOG_MODULE_DECLARE(wifimgr);
 
 #include "wifimgr.h"
 
-static struct wifimgr_ctrl_cbs *wifimgr_cbs = NULL;
-
-static int wifimgr_ctrl_iface_send_cmd(unsigned int cmd_id, void *buf,
-				       int buf_len)
-{
-	struct mq_des *mq;
-	struct mq_attr attr;
-	struct cmd_message msg;
-	struct timespec ts;
-	int prio;
-	int ret;
-
-	attr.mq_maxmsg = WIFIMGR_CMD_MQUEUE_NR;
-	attr.mq_msgsize = sizeof(msg);
-	attr.mq_flags = 0;
-	mq = mq_open(WIFIMGR_CMD_MQUEUE, O_RDWR | O_CREAT, 0666, &attr);
-	if (!mq) {
-		wifimgr_err("failed to open command queue %s!\n",
-			    WIFIMGR_CMD_MQUEUE);
-		return -errno;
-	}
-
-	msg.cmd_id = cmd_id;
-	msg.reply = 0;
-	msg.buf_len = buf_len;
-	msg.buf = NULL;
-	if (buf_len) {
-		msg.buf = malloc(buf_len);
-		memcpy(msg.buf, buf, buf_len);
-	}
-
-	ret = mq_send(mq, (const char *)&msg, sizeof(msg), 0);
-	if (ret < 0) {
-		wifimgr_err("failed to send [%s]: %d, errno %d!\n",
-			    wifimgr_cmd2str(msg.cmd_id), ret, errno);
-		ret = -errno;
-	} else {
-		wifimgr_dbg("send [%s], buf: 0x%08x\n",
-			    wifimgr_cmd2str(msg.cmd_id), *(int *)msg.buf);
-
-		ret = clock_gettime(CLOCK_MONOTONIC, &ts);
-		if (ret)
-			wifimgr_err("failed to get clock time: %d!\n", ret);
-		ts.tv_sec += WIFIMGR_CMD_TIMEOUT;
-		ret =
-		    mq_timedreceive(mq, (char *)&msg, sizeof(msg), &prio, &ts);
-		if (ret != sizeof(struct cmd_message)) {
-			wifimgr_err
-			    ("failed to get command reply: %d, errno %d!\n",
-			     ret, errno);
-			if (errno == ETIME)
-				wifimgr_err("[%s] timeout!\n",
-					    wifimgr_cmd2str(msg.cmd_id));
-			ret = -errno;
-		} else {
-			wifimgr_dbg("recv [%s] reply: %d\n",
-				    wifimgr_cmd2str(msg.cmd_id), msg.reply);
-			ret = msg.reply;
-			if (ret)
-				wifimgr_err("failed to exec [%s]: %d!\n",
-					    wifimgr_cmd2str(msg.cmd_id), ret);
-		}
-	}
-
-	free(msg.buf);
-	mq_close(mq);
-
-	return ret;
-}
+static struct wifimgr_ctrl_cbs *wifimgr_cbs;
 
 int wifimgr_ctrl_iface_set_conf(char *iface_name, char *ssid, char *bssid,
 				char *passphrase, unsigned char band,
@@ -120,7 +52,7 @@ int wifimgr_ctrl_iface_set_conf(char *iface_name, char *ssid, char *bssid,
 	else
 		return -EINVAL;
 
-	return wifimgr_ctrl_iface_send_cmd(cmd_id, &conf, sizeof(conf));
+	return wifimgr_send_cmd(cmd_id, &conf, sizeof(conf));
 }
 
 int wifimgr_ctrl_iface_get_conf(char *iface_name)
@@ -137,7 +69,7 @@ int wifimgr_ctrl_iface_get_conf(char *iface_name)
 	else
 		return -EINVAL;
 
-	return wifimgr_ctrl_iface_send_cmd(cmd_id, NULL, 0);
+	return wifimgr_send_cmd(cmd_id, NULL, 0);
 }
 
 int wifimgr_ctrl_iface_get_status(char *iface_name)
@@ -154,7 +86,7 @@ int wifimgr_ctrl_iface_get_status(char *iface_name)
 	else
 		return -EINVAL;
 
-	return wifimgr_ctrl_iface_send_cmd(cmd_id, NULL, 0);
+	return wifimgr_send_cmd(cmd_id, NULL, 0);
 }
 
 int wifimgr_ctrl_iface_open(char *iface_name)
@@ -171,7 +103,7 @@ int wifimgr_ctrl_iface_open(char *iface_name)
 	else
 		return -EINVAL;
 
-	return wifimgr_ctrl_iface_send_cmd(cmd_id, NULL, 0);
+	return wifimgr_send_cmd(cmd_id, NULL, 0);
 }
 
 int wifimgr_ctrl_iface_close(char *iface_name)
@@ -188,32 +120,32 @@ int wifimgr_ctrl_iface_close(char *iface_name)
 	else
 		return -EINVAL;
 
-	return wifimgr_ctrl_iface_send_cmd(cmd_id, NULL, 0);
+	return wifimgr_send_cmd(cmd_id, NULL, 0);
 }
 
 int wifimgr_ctrl_iface_scan(void)
 {
-	return wifimgr_ctrl_iface_send_cmd(WIFIMGR_CMD_SCAN, NULL, 0);
+	return wifimgr_send_cmd(WIFIMGR_CMD_SCAN, NULL, 0);
 }
 
 int wifimgr_ctrl_iface_connect(void)
 {
-	return wifimgr_ctrl_iface_send_cmd(WIFIMGR_CMD_CONNECT, NULL, 0);
+	return wifimgr_send_cmd(WIFIMGR_CMD_CONNECT, NULL, 0);
 }
 
 int wifimgr_ctrl_iface_disconnect(void)
 {
-	return wifimgr_ctrl_iface_send_cmd(WIFIMGR_CMD_DISCONNECT, NULL, 0);
+	return wifimgr_send_cmd(WIFIMGR_CMD_DISCONNECT, NULL, 0);
 }
 
 int wifimgr_ctrl_iface_start_ap(void)
 {
-	return wifimgr_ctrl_iface_send_cmd(WIFIMGR_CMD_START_AP, NULL, 0);
+	return wifimgr_send_cmd(WIFIMGR_CMD_START_AP, NULL, 0);
 }
 
 int wifimgr_ctrl_iface_stop_ap(void)
 {
-	return wifimgr_ctrl_iface_send_cmd(WIFIMGR_CMD_STOP_AP, NULL, 0);
+	return wifimgr_send_cmd(WIFIMGR_CMD_STOP_AP, NULL, 0);
 }
 
 int wifimgr_ctrl_iface_del_station(char *mac)
@@ -225,8 +157,8 @@ int wifimgr_ctrl_iface_del_station(char *mac)
 	else
 		memset(del_sta.mac, 0xff, WIFIMGR_ETH_ALEN);
 
-	return wifimgr_ctrl_iface_send_cmd(WIFIMGR_CMD_DEL_STATION, &del_sta,
-					   sizeof(del_sta));
+	return wifimgr_send_cmd(WIFIMGR_CMD_DEL_STATION, &del_sta,
+				sizeof(del_sta));
 }
 
 static const struct wifimgr_ctrl_ops wifimgr_ops = {
