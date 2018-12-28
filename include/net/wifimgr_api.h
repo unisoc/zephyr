8e0ceb240edf21a5b73d000641928795c183fc41
Keguang Zhang <keguang.zhang@unisoc.com>
2018-12-28 12:58:32 +0800

    include: net: add autorun to config
    - Add autorun to config
    - Rename channel_width to ch_width
    - Split get_conf_cb into get_sta_conf_cb and get_ap_conf_cb.
    
    Signed-off-by: Keguang Zhang <keguang.zhang@unisoc.com>

--- include/net/wifimgr_api.h
+++ include/net/wifimgr_api.h
@@ -18,7 +18,8 @@
 struct wifimgr_ctrl_ops {
 	int (*set_conf) (char *iface_name, char *ssid, char *bssid,
 			 char *passphrase, unsigned char band,
-			 unsigned char channel, unsigned char channel_width);
+			 unsigned char channel, unsigned char channel_width,
+			 char autorun);
 	int (*get_conf) (char *iface_name);
 	int (*get_status) (char *iface_name);
 	int (*open) (char *iface_name);
@@ -32,10 +33,12 @@ struct wifimgr_ctrl_ops {
 };
 
 struct wifimgr_ctrl_cbs {
-	void (*get_conf_cb) (char *iface_name, char *ssid, char *bssid,
-			     char *passphrase, unsigned char band,
-			     unsigned char channel,
-			     unsigned char channel_width);
+	void (*get_sta_conf_cb) (char *ssid, char *bssid, char *passphrase,
+				 unsigned char band, unsigned char channel,
+				 char autorun);
+	void (*get_ap_conf_cb) (char *ssid, char *passphrase,
+				unsigned char band, unsigned char channel,
+				unsigned char channel_width, char autorun);
 	void (*get_sta_status_cb) (unsigned char status, char *own_mac,
 				   char *host_ssid, char *host_bssid,
 				   char host_channel, char host_rssi);
