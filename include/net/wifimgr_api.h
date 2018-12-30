effb2b756f4cc1b7bda73695f41354d281ac3a24
Keguang Zhang <keguang.zhang@unisoc.com>
2018-12-30 19:10:34 +0800

    include: net: refactor WiFi manager header files
    Refactor WiFi manager header files.
    
    Signed-off-by: Keguang Zhang <keguang.zhang@unisoc.com>

--- include/net/wifimgr_api.h
+++ include/net/wifimgr_api.h
@@ -16,44 +16,44 @@
 #define WIFIMGR_IFACE_NAME_AP	"ap"
 
 struct wifimgr_ctrl_ops {
-	int (*set_conf) (char *iface_name, char *ssid, char *bssid,
-			 char *passphrase, unsigned char band,
-			 unsigned char channel, unsigned char channel_width,
-			 char autorun);
-	int (*get_conf) (char *iface_name);
-	int (*get_status) (char *iface_name);
-	int (*open) (char *iface_name);
-	int (*close) (char *iface_name);
-	int (*scan) (void);
-	int (*connect) (void);
-	int (*disconnect) (void);
-	int (*start_ap) (void);
-	int (*stop_ap) (void);
-	int (*del_station) (char *mac);
+	int (*set_conf)(char *iface_name, char *ssid, char *bssid,
+			char *passphrase, unsigned char band,
+			unsigned char channel, unsigned char channel_width,
+			char autorun);
+	int (*get_conf)(char *iface_name);
+	int (*get_status)(char *iface_name);
+	int (*open)(char *iface_name);
+	int (*close)(char *iface_name);
+	int (*scan)(void);
+	int (*connect)(void);
+	int (*disconnect)(void);
+	int (*start_ap)(void);
+	int (*stop_ap)(void);
+	int (*del_station)(char *mac);
 };
 
 struct wifimgr_ctrl_cbs {
-	void (*get_sta_conf_cb) (char *ssid, char *bssid, char *passphrase,
-				 unsigned char band, unsigned char channel,
-				 char autorun);
-	void (*get_ap_conf_cb) (char *ssid, char *passphrase,
+	void (*get_sta_conf_cb)(char *ssid, char *bssid, char *passphrase,
 				unsigned char band, unsigned char channel,
-				unsigned char channel_width, char autorun);
-	void (*get_sta_status_cb) (unsigned char status, char *own_mac,
-				   char *host_ssid, char *host_bssid,
-				   char host_channel, char host_rssi);
-	void (*get_ap_status_cb) (unsigned char status, char *own_mac,
-				  char client_nr, char client_mac[][6]);
-	void (*notify_scan_res) (char *ssid, char *bssid, unsigned char band,
-				 unsigned char channel, char signal);
-	void (*notify_scan_done) (unsigned char result);
-	void (*notify_connect) (unsigned char result);
-	void (*notify_disconnect) (unsigned char reason);
-	void (*notify_scan_timeout) (void);
-	void (*notify_connect_timeout) (void);
-	void (*notify_disconnect_timeout) (void);
-	void (*notify_new_station) (unsigned char status, unsigned char *mac);
-	void (*notify_del_station_timeout) (void);
+				char autorun);
+	void (*get_ap_conf_cb)(char *ssid, char *passphrase,
+			       unsigned char band, unsigned char channel,
+			       unsigned char channel_width, char autorun);
+	void (*get_sta_status_cb)(unsigned char status, char *own_mac,
+				  char *host_ssid, char *host_bssid,
+				  char host_channel, char host_rssi);
+	void (*get_ap_status_cb)(unsigned char status, char *own_mac,
+				 char client_nr, char client_mac[][6]);
+	void (*notify_scan_res)(char *ssid, char *bssid, unsigned char band,
+				unsigned char channel, char signal);
+	void (*notify_scan_done)(unsigned char result);
+	void (*notify_connect)(unsigned char result);
+	void (*notify_disconnect)(unsigned char reason);
+	void (*notify_scan_timeout)(void);
+	void (*notify_connect_timeout)(void);
+	void (*notify_disconnect_timeout)(void);
+	void (*notify_new_station)(unsigned char status, unsigned char *mac);
+	void (*notify_del_station_timeout)(void);
 };
 
 const
