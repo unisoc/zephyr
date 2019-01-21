658c2f5a653c21e981bf6ae3fb94466b3ef0ebc5
Keguang Zhang <keguang.zhang@unisoc.com>
2019-01-21 16:57:45 +0800

    include: net: enhance autorun to support periodic autorun
    Enhance autorun to support periodic autorun.
    
    Signed-off-by: Keguang Zhang <keguang.zhang@unisoc.com>

--- include/net/wifimgr_api.h
+++ include/net/wifimgr_api.h
@@ -22,11 +22,12 @@ enum wifimgr_security {
 };
 
 struct wifimgr_ctrl_ops {
+	int (*get_conf)(char *iface_name);
 	int (*set_conf)(char *iface_name, char *ssid, char *bssid,
 			char security, char *passphrase, unsigned char band,
 			unsigned char channel, unsigned char channel_width,
-			char autorun);
-	int (*get_conf)(char *iface_name);
+			int autorun);
+	int (*clear_conf)(char *iface_name);
 	int (*get_capa)(char *iface_name);
 	int (*get_status)(char *iface_name);
 	int (*open)(char *iface_name);
@@ -42,10 +43,10 @@ struct wifimgr_ctrl_ops {
 struct wifimgr_ctrl_cbs {
 	void (*get_sta_conf_cb)(char *ssid, char *bssid, char *passphrase,
 				unsigned char band, unsigned char channel,
-				enum wifimgr_security security, char autorun);
+				enum wifimgr_security security, int autorun);
 	void (*get_ap_conf_cb)(char *ssid, char *passphrase, unsigned char band,
 			       unsigned char channel, unsigned char ch_width,
-			       enum wifimgr_security security, char autorun);
+			       enum wifimgr_security security, int autorun);
 	void (*get_ap_capa_cb)(unsigned char max_sta, unsigned char max_acl);
 	void (*get_sta_status_cb)(char status, char *own_mac, char *host_bssid,
 				  signed char host_rssi);
