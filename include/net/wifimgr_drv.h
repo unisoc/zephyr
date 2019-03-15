fbf7273d893df0dda27854dc923c460eb54cd581
Keguang Zhang <keguang.zhang@unisoc.com>
2019-03-15 11:04:05 +0800

    include: net: refactor WiFi manager API
    - Refactor WiFi manager API to provide sync interface for users.
    - Add RTT range request function.
    
    Signed-off-by: Keguang Zhang <keguang.zhang@unisoc.com>

--- include/net/wifimgr_drv.h
+++ include/net/wifimgr_drv.h
@@ -13,15 +13,19 @@
 #define _WIFIMGR_DRV_H_
 
 #include <net/ethernet.h>
-#include <net/net_linkaddr.h>
 #include <net/wifi.h>
 
 #define WIFI_DRV_BLACKLIST_ADD	(1)
 #define WIFI_DRV_BLACKLIST_DEL	(2)
 
-struct wifi_drv_capa {
-	unsigned char max_ap_assoc_sta;
-	unsigned char max_acl_mac_addrs;
+union wifi_drv_capa {
+	struct {
+		unsigned char max_rtt_peers;
+	} sta;
+	struct {
+		unsigned char max_ap_assoc_sta;
+		unsigned char max_acl_mac_addrs;
+	} ap;
 };
 
 struct wifi_drv_scan_params {
@@ -51,18 +55,19 @@ struct wifi_drv_start_ap_params {
 	char security;
 };
 
+struct wifi_drv_rtt_request {
+	unsigned char nr_peers;
+	struct wifi_rtt_peers {
+		char bssid[NET_LINK_ADDR_MAX_LENGTH];
+		unsigned char band;
+		unsigned char channel;
+	} *peers;
+};
+
 struct wifi_drv_connect_evt {
 	char status;
 	char bssid[NET_LINK_ADDR_MAX_LENGTH];
 	unsigned char channel;
-};
-
-struct wifi_drv_disconnect_evt {
-	char reason_code;
-};
-
-struct wifi_drv_scan_done_evt {
-	char result;
 };
 
 struct wifi_drv_scan_result_evt {
@@ -73,6 +78,12 @@ struct wifi_drv_scan_result_evt {
 	unsigned char channel;
 	signed char rssi;
 	char security;
+	char rtt_supported;
+};
+
+struct wifi_drv_rtt_response_evt {
+	char bssid[NET_LINK_ADDR_MAX_LENGTH];
+	int range;
 };
 
 struct wifi_drv_new_station_evt {
@@ -80,12 +91,14 @@ struct wifi_drv_new_station_evt {
 	char mac[NET_LINK_ADDR_MAX_LENGTH];
 };
 
-typedef void (*scan_result_cb_t)(void *iface, int status,
+typedef void (*scan_result_evt_t)(void *iface, int status,
 				 struct wifi_drv_scan_result_evt *entry);
-typedef void (*connect_cb_t)(void *iface, int status, char *bssid,
+typedef void (*rtt_result_evt_t)(void *iface, int status,
+				 struct wifi_drv_rtt_response_evt *entry);
+typedef void (*connect_evt_t)(void *iface, int status, char *bssid,
 			     unsigned char channel);
-typedef void (*disconnect_cb_t)(void *iface, int status);
-typedef void (*new_station_t)(void *iface, int status, char *mac);
+typedef void (*disconnect_evt_t)(void *iface, int status);
+typedef void (*new_station_evt_t)(void *iface, int status, char *mac);
 
 struct wifi_drv_api {
 	/**
@@ -96,20 +109,22 @@ struct wifi_drv_api {
 	 */
 	struct ethernet_api eth_api;
 
-	int (*get_capa)(struct device *dev, struct wifi_drv_capa *capa);
+	int (*get_capa)(struct device *dev, union wifi_drv_capa *capa);
 	int (*open)(struct device *dev);
 	int (*close)(struct device *dev);
 	int (*scan)(struct device *dev, struct wifi_drv_scan_params *params,
-		    scan_result_cb_t cb);
+		    scan_result_evt_t cb);
+	int (*rtt_req)(struct device *dev, struct wifi_drv_rtt_request *params,
+		    rtt_result_evt_t cb);
 	int (*connect)(struct device *dev,
 		       struct wifi_drv_connect_params *params,
-		       connect_cb_t conn_cb, disconnect_cb_t disc_cb);
-	int (*disconnect)(struct device *dev, disconnect_cb_t cb);
+		       connect_evt_t conn_cb, disconnect_evt_t disc_cb);
+	int (*disconnect)(struct device *dev, disconnect_evt_t cb);
 	int (*get_station)(struct device *dev, signed char *rssi);
 	int (*notify_ip)(struct device *dev, char *ipaddr, unsigned char len);
 	int (*start_ap)(struct device *dev,
 			struct wifi_drv_start_ap_params *params,
-			new_station_t cb);
+			new_station_evt_t cb);
 	int (*stop_ap)(struct device *dev);
 	int (*del_station)(struct device *dev, char *mac);
 	int (*set_mac_acl)(struct device *dev, char subcmd,
