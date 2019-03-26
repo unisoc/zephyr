c3dd68d96e6776d83583bc3489c386f12258abff
Hazel Zhang <hazel.zhang@unisoc.com>
2019-03-26 14:28:57 +0800

    implement watchdog driver code
    implement watchdog driver code
    
    Signed-off-by: Hazel Zhang <hazel.zhang@unisoc.com>

--- drivers/watchdog/wdt_uwp.c
+++ drivers/watchdog/wdt_uwp.c
@@ -8,17 +8,17 @@
 #include <soc.h>
 #include <errno.h>
 #include <assert.h>
+#include <kernel.h>
 
 #include "uwp_hal.h"
 
 #define DEV_CFG(dev) \
-	((const struct wdg_uwp_config * const)(dev)->config->config_info)
+	((const struct wdg_uwp_config *)(dev)->config->config_info)
 #define DEV_DATA(dev) \
 	((struct wdg_uwp_data *)(dev)->driver_data)
 
-static void uwp_config_irq(struct device *dev);
 struct wdg_uwp_data {
-	void (*cb)(struct device *dev);
+	wdt_callback_t cb;
 	u32_t timeout;
 	u32_t reload;
 	u32_t mode;
@@ -29,18 +29,6 @@ struct wdg_uwp_config {
 };
 
 static struct device DEVICE_NAME_GET(wdg_uwp);
-static struct wdg_uwp_data uwp_dev_data = {
-	.cb = NULL,
-};
-
-static struct wdg_uwp_config uwp_dev_cfg = {
-	.irq_config_func = uwp_config_irq,
-};
-
-static void wdg_uwp_enable(struct device *dev)
-{
-	uwp_wdg_enable();
-}
 
 static int wdg_uwp_disable(struct device *dev)
 {
@@ -49,52 +37,62 @@ static int wdg_uwp_disable(struct device *dev)
 	return 0;
 }
 
-static void wdg_uwp_reload(struct device *dev)
+static int wdg_uwp_install_timeout(struct device *dev,
+				      const struct wdt_timeout_cfg *cfg)
 {
 	struct wdg_uwp_data * const dev_data = DEV_DATA(dev);
+	ARG_UNUSED(dev);
+	if (cfg->flags != WDT_FLAG_RESET_SOC) {
+		return -ENOTSUP;
+	}
 
-	uwp_wdg_load(dev_data->timeout);
-	uwp_wdg_load_irq(dev_data->timeout >> 1);
+	if (cfg->window.min != 0 || cfg->window.max == 0) {
+		return -EINVAL;
+	}
+
+	dev_data->timeout = cfg->window.max;
+
+	dev_data->mode = (cfg->callback == NULL) ?
+			WDT_MODE_RESET : WDT_MODE_INTERRUPT_RESET;
+	dev_data->cb = cfg->callback;
+	return 0;
 }
 
-static int wdg_uwp_set_config(struct device *dev,
-				 struct wdt_config *config)
+/*  channel_id: irq timeout  */
+static int wdg_uwp_feed(struct device *dev, int channel_id)
 {
 	struct wdg_uwp_data * const dev_data = DEV_DATA(dev);
-	u32_t mode;
+	uwp_wdg_load(dev_data->timeout);
+	uwp_wdg_load_irq(channel_id);
 
-	if (config->mode == WDT_MODE_RESET)
-		mode = WDG_MODE_RESET;
-	else
-		mode = WDG_MODE_MIX;
-
-	dev_data->mode = config->mode;
-	dev_data->timeout = config->timeout;
-	dev_data->cb = config->interrupt_fn;
-
-	uwp_wdg_set_mode(mode);
-	wdg_uwp_reload(dev);
 
 	return 0;
 }
 
-static void wdg_uwp_get_config(struct device *dev,
-				  struct wdt_config *config)
+/*  options: irq timeout  */
+static int wdg_uwp_setup(struct device *dev, u8_t options)
 {
 	struct wdg_uwp_data * const dev_data = DEV_DATA(dev);
+	u32_t mode;
 
-	config->timeout = dev_data->timeout;
-	config->mode = dev_data->mode;
-	config->interrupt_fn = dev_data->cb;
+	if (dev_data->mode == WDT_MODE_RESET)
+		mode = WDG_MODE_RESET;
+	else
+		mode = WDG_MODE_MIX;
+	uwp_wdg_set_mode(mode);
+	wdg_uwp_feed(dev, options);
+
+	uwp_wdg_enable();
+
+	return 0;
 }
 
 static void wdg_uwp_isr(void *arg)
 {
 	struct device *dev = arg;
 	struct wdg_uwp_data * const dev_data = DEV_DATA(dev);
-
 	if (dev_data->cb) {
-		dev_data->cb(dev);
+		dev_data->cb(dev, 0);
 	}
 
 	/* Clear interrupts here */
@@ -102,11 +100,17 @@ static void wdg_uwp_isr(void *arg)
 }
 
 static const struct wdt_driver_api wdg_uwp_api = {
-	.enable = wdg_uwp_enable,
+	.setup = wdg_uwp_setup,
 	.disable = wdg_uwp_disable,
-	.get_config = wdg_uwp_get_config,
-	.set_config = wdg_uwp_set_config,
-	.reload = wdg_uwp_reload,
+	.install_timeout = wdg_uwp_install_timeout,
+	.feed = wdg_uwp_feed,
+};
+
+#ifdef CONFIG_WDT_UWP
+
+static struct wdg_uwp_data uwp_dev_data = {
+	.cb = NULL,
+	.timeout = 3000,
 };
 
 static void uwp_config_irq(struct device *dev)
@@ -117,10 +121,13 @@ static void uwp_config_irq(struct device *dev)
 	irq_enable(DT_WDT_UWP_IRQ);
 }
 
+static struct wdg_uwp_config uwp_dev_cfg = {
+	.irq_config_func = uwp_config_irq,
+};
+
 static int wdg_uwp_init(struct device *dev)
 {
 	const struct wdg_uwp_config * const config = DEV_CFG(dev);
-
 	uwp_sys_enable(BIT(APB_EB_WDG)
 			| BIT(APB_EB_WDG_RTC)
 			| BIT(APB_EB_SYST_RTC));
@@ -133,7 +140,8 @@ static int wdg_uwp_init(struct device *dev)
 }
 
 DEVICE_AND_API_INIT(wdg_uwp, DT_WDT_UWP_DEVICE_NAME,
-		    wdg_uwp_init, &uwp_dev_data, &uwp_dev_cfg,
+		    &wdg_uwp_init, &uwp_dev_data, &uwp_dev_cfg,
 		    POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    &wdg_uwp_api);
+#endif
 
